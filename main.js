b68ea3a98dce258f068ae5e2a20cd37fa7662186
nguyen van thai <nguyenvanthailtv@gmail.com>
2025-03-19 14:13:44 +0700

    .

--- main.js
+++ main.js
@@ -29,18 +29,18 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     productList.addEventListener('touchend', () => {
-        slide = 0
-        productItems.forEach(element => {
-            const item = element.getBoundingClientRect();
-            if(item.left < 0 && item.left + item.width < item.width / 2){
-                slide = slide + 1
-            }
+        // slide = 0
+        // productItems.forEach(element => {
+        //     const item = element.getBoundingClientRect();
+        //     if(item.left < 0 && item.left + item.width < item.width / 2){
+        //         slide = slide + 1
+        //     }
             
-        });
-        productList.scrollTo({
-            // left: slide - 1 > 0 ? slide * 200 + 8 * (slide - 1) : slide * 200,
-            left: slide * 200 + 8 * slide,
-            behavior: "smooth",
-        })
+        // });
+        // productList.scrollTo({
+        //     // left: slide - 1 > 0 ? slide * 200 + 8 * (slide - 1) : slide * 200,
+        //     left: slide * 200 + 8 * slide,
+        //     behavior: "smooth",
+        // })
     })
 })
