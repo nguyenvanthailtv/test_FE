9be31f2b285c32c6caab881772234612548b4d65
nguyen van thai <nguyenvanthailtv@gmail.com>
2025-03-19 14:55:11 +0700

    .

--- main.js
+++ main.js
@@ -2,16 +2,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     const productList = document.querySelector('.product-list')
     let slide = 0
+    let width = 0
+    let remove = true
 
+    const cards = Array.from({ length: 40 }, (_, i) => `product-item ${i + 1}`);
+
+    // Thêm card vào DOM
+    cards.forEach(cardText => {
+      const card = document.createElement("div");
+      card.className = "product-item";
+    //   card.textContent = cardText;
+
+      const cardContent = document.createElement('div')
+      cardContent.textContent = cardText
+      card.appendChild(cardContent)
+      productList.appendChild(card);
+    });
     const productItems = document.querySelectorAll('.product-item')
-    // productList.addEventListener('scroll', (event) => {
-    //     console.log(productList.scrollLeft,'event')
-    // })
+    productList.addEventListener('scroll', (event) => {
+       
+    })
 
     productList.addEventListener('scrollend', () => {
         slide = 0
         productItems.forEach(element => {
             const item = element.getBoundingClientRect();
+
+            width = item.width
             if(item.left < 0 && item.left + item.width < item.width / 2){
                 slide = slide + 1
             }
@@ -19,9 +36,18 @@ document.addEventListener('DOMContentLoaded', () => {
         });
         productList.scrollTo({
             // left: slide - 1 > 0 ? slide * 200 + 8 * (slide - 1) : slide * 200,
-            left: slide * 200 + 8 * slide,
+            left: slide * width ,
             behavior: "smooth",
         })
+
+        productItems.forEach(element => {
+            const item = element.getBoundingClientRect();
+            if(item.left > 0 && item.left < item.width) {
+                element.classList.add('active')
+            }
+            
+        });
+
     })
 
     productList.addEventListener('touchstart', () => {
